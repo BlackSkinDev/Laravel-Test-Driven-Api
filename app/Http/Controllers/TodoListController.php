aef4e30a8cde6d7793bef704ac3853654958bb2d
chillingloccini <chillingloccini1@gmail.com>
2022-07-05 18:37:59 +0100

    Created test for fetching all todo lists, fetching single todo list and storing new todo list"

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -2,48 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoListRequest;
+use App\Models\TodoList;
+use App\Traits\ApiResponse;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+use Tests\Feature\TodoListTest;
 
 class TodoListController extends Controller
 {
+    use ApiResponse;
+
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     *
+     *
+     *
+     * @return JsonResponse
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        return  response(['lists'=>[]]);
+        $lists  = TodoList::all();
+        return  $this->success($lists,null,Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param TodoListRequest $request
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function store(TodoListRequest $request): JsonResponse
     {
-        //
+        $list = TodoList::create($request->validated());
+        return  $this->success($list, null,Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param TodoList $todolist
+     * @return JsonResponse
      */
-    public function show($id)
+    public function show(TodoList $list): JsonResponse
     {
-        //
+        return $this->success($list,null,Response::HTTP_OK);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, $id)
     {
@@ -54,7 +68,7 @@ class TodoListController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy($id)
     {
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('todo-list',[App\Http\Controllers\TodoListController::class,'index'])->name('todo-list.store');
+Route::get('todo-list',[App\Http\Controllers\TodoListController::class,'index'])->name('todo-list.index');
+Route::get('todo-list/{list}',[App\Http\Controllers\TodoListController::class,'show'])->name('todo-list.show');
+Route::post('todo-list',[App\Http\Controllers\TodoListController::class,'store'])->name('todo-list.store');
